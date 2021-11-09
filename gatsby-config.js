f0ca6cab14395fc805691c844c0040a2436d6445
Mike <30309193+melansonwrites@users.noreply.github.com>
2021-11-09 16:42:13 -0500

    changes to siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `About Mike Melanson`,
+    description: `Mike Melanson is a writer, cyclist, lifelong techie, and general outdoor afficianado.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `http://mikemelanson.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
