df3aeb2c3ab2641a58539a749b6448bb844be17b
Mike <30309193+melansonwrites@users.noreply.github.com>
2021-11-28 14:21:57 -0500

    added layout stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,26 @@
 // Step 1: Import React
 import * as React from 'react'
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
+    <Layout>
+    <Seo title="Mike Melanson - Writer, Cyclist, Techie" />
+    <StaticImage 
+      src="../images/mike-melanson.jpg"
+      quality={95}
+      alt="A beardless (rare) Mike Melanson"
+      style={{ marginBottom: `1.45rem` }}
+    />
+   <main>
       <title>About Me</title>
       <h1>About Me</h1>
       <p>I am Mike, AKA charioteer of Mabel the dog.</p>
     </main>
+    </Layout>
   )
 }
 
