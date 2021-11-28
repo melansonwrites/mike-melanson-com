9150e9d9a6c306f42638feb36f7c4d2d5fc63e55
Mike <30309193+melansonwrites@users.noreply.github.com>
2021-11-28 14:10:03 -0500

    created about page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>I am Mike, AKA charioteer of Mabel the dog.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
